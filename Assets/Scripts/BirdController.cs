a870869eb6ac4af069eb1979566473e7e3ea1915
grzegorztomaszewski <aroxd3@gmail.com>
2018-11-29 21:17:35 +0100

    flappy bird 2D

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour {
+
+    public float flapForce;
+    public Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(Vector2.up * flapForce);
+        }
+	}
+}
---
+++ Assets/Scripts/Spwaner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spwaner : MonoBehaviour {
+    public GameObject pipePrefab;
+    public float spawnDelay;
+    public Vector3 spawnVector;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(spawnPipeCoroutine());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //waiting
+    public IEnumerator spawnPipeCoroutine()
+    {
+        while (true)
+        {
+            spawnVector = new Vector3(transform.position.x, Random.Range(-0.3f, 0.85f), 0);
+            Instantiate(pipePrefab, spawnVector, Quaternion.identity);
+            //Debug.Log("Spawn");
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+}
